3b2e03cfafaf1f9ea18b171cc4735f26ff99650e
Colin Wood <cwood06@gmail.com>
2015-07-29 16:24:20 -0700

    Dont make this a self. and just use it locally

--- lib/razor/data/node.rb
+++ lib/razor/data/node.rb
@@ -212,7 +212,8 @@ module Razor::Data
       self.hostname = ERB.new(
           policy.hostname_pattern.gsub(/\$\{\s*id\s*\}/, id.to_s)
       ).result(binding)
-      self.current_policy_count = Node.dataset.filter(:policy_id => self.policy.id).count() + 1
+
+      current_policy_count = Node.dataset.filter(:policy_id => self.policy.id).count() + 1
 
       if policy.node_metadata
         modify_metadata('no_replace' => true, 'update' => policy.node_metadata)
